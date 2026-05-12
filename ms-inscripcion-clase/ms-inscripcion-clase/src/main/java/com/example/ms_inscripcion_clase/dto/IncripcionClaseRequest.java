9b0e5bbade5e458db73266d22a49ccb5c819c145
rrrbbb20 <rodr.baez@duocuc.cl>
2026-05-12 17:36:29 -0400

    ms-inscripcion add validations dto request

---
+++ ms-inscripcion-clase/ms-inscripcion-clase/src/main/java/com/example/ms_inscripcion_clase/dto/IncripcionClaseRequest.java
@@ -0,0 +1,19 @@
+package com.example.ms_inscripcion_clase.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+public class IncripcionClaseRequest {
+
+    @NotNull(message = "Debe ingresar id de la clase")
+    private Long idClase;
+    @NotNull(message = "Debe ingresar id del inscrito")
+    private Long idCliente;
+    private LocalDate fechaInscripcion;
+    private LocalTime horaInscripcion;
+
+}
