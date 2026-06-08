e14cf3c3efe9f74f0bbf7a357eea7ff3e890eeac
krist41 <kristine.spl@proton.me>
2026-06-08 11:00:11 -0400

    metodo de agregarcliente en la capa de servicio agregado, y corregido el repository de cliente

--- ms-cliente/src/main/java/com/proyectogimnasio/cliente/service/ClienteService.java
+++ ms-cliente/src/main/java/com/proyectogimnasio/cliente/service/ClienteService.java
@@ -5,6 +5,7 @@ import com.proyectogimnasio.cliente.dto.ClienteRequest;
 import com.proyectogimnasio.cliente.dto.ClienteResponse;
 import com.proyectogimnasio.cliente.model.Cliente;
 import com.proyectogimnasio.cliente.repository.ClienteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,32 @@ import static net.logstash.logback.argument.StructuredArguments.keyValue;
 public class ClienteService {
     private final ClienteRepository repo;
     private final PlanesClient client;
+
+    //metodo para agregar clientes
+    private ClienteResponse add(ClienteRequest c, String token){
+        log.info("Anadir Cliente", keyValue("cliente", c.getNombres()));//cuando se agrega un cliente se usa el log para poder dejar guardada la accion
+        var plan = client.getPlan(c.getIdPlan(), token);
+        //si es que el plan no existe, el metodo va a advertir de ello y no va a seguir
+        if(plan == null){
+            log.warn("Plan no existe",
+                keyValue("idPlan",c.getIdPlan()));
+            throw new EntityNotFoundException("Plan no encontrado");
+        }
+        //datos del cliente
+        Cliente cliente1 = new Cliente();
+        cliente1.setNombres(c.getNombres());
+        cliente1.setApellidos(c.getApellidos());
+        cliente1.setRun(c.getRun());
+        cliente1.setCorreo(c.getCorreo());
+        cliente1.setIdPlan(c.getIdPlan());
+        Cliente saveCliente = repo.save(cliente1);
+        log.info("Cliente creado correctamente",
+                keyValue("idCliente", saveCliente.getId()));
+        return mapToResponse(saveCliente,token);
+
+
+    }
+
 
 
 //mapToResponse sirve para que todo quede ordenado o mapeado para que cuando se retorne, muestre los datos del cliente
