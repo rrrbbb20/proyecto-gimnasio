6baa2d68882d10f29111e1b7c02bd5b1bf25b06f
krist41 <kristine.spl@proton.me>
2026-06-08 09:56:04 -0400

    PlanesResponse Listo

--- ms-cliente/src/main/java/com/proyectogimnasio/cliente/dto/PlanesResponse.java
+++ ms-cliente/src/main/java/com/proyectogimnasio/cliente/dto/PlanesResponse.java
@@ -1,4 +1,10 @@
 package com.proyectogimnasio.cliente.dto;
 
+import com.example.ms_planes.model.Pagos;
+
 public class PlanesResponse {
+    private Long id;
+    private String nombrePlan;
+    private Integer precioPlan;
+    private Pagos Pagos;
 }
