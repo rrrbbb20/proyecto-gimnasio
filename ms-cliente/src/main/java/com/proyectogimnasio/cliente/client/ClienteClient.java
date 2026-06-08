b3485e617966957f2eeead2c842c1f3c70b59b81
krist41 <kristine.spl@proton.me>
2026-06-08 09:38:00 -0400

    El cliente de conexion a ms_planes en ms_cliente creado y configurado

---
+++ ms-cliente/src/main/java/com/proyectogimnasio/cliente/client/ClienteClient.java
@@ -0,0 +1,28 @@
+package com.proyectogimnasio.cliente.client;
+
+
+import com.example.ms_planes.dto.ApiResponse;
+import com.example.ms_planes.dto.PlanesResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class ClienteClient {
+    private final WebClient webClient;
+
+    private final String BASE_URL = "http://localhost:8087/api/v2/planes/";
+
+    public PlanesResponse getEntrenador(Long id, String token) {
+
+        ApiResponse<PlanesResponse> response = webClient.get()
+                .uri(BASE_URL + id)
+                .header("Authorization", token)
+                .retrieve()
+                .bodyToMono(new org.springframework.core.ParameterizedTypeReference<ApiResponse<PlanesResponse>>() {})
+                .block();
+
+        return response != null ? response.getData() : null;
+    }
+}
