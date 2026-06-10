d03b71899f17c23b64733468c37204bb275ce3e6
krist41 <kristine.spl@proton.me>
2026-06-10 09:56:11 -0400

    configurado los request y response de Planes

--- ms_planes/src/main/java/com/proyectogimnasio/planes/dto/PlanesRequest.java
+++ ms_planes/src/main/java/com/proyectogimnasio/planes/dto/PlanesRequest.java
@@ -1,4 +1,22 @@
 package com.proyectogimnasio.planes.dto;
 
+import com.proyectogimnasio.planes.model.Pagos;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class PlanesRequest {
+    @NotBlank(message = "El nombre no puede quedar vacio")
+    private String nombrePlan;
+    @NotNull(message = "Debe ingresar el precio del plan")
+    private Integer precioPlan;
+    @NotBlank(message = "Debe agregar una descripcion valida")
+    private String descripcionPlan;
+    @NotBlank(message = "Debe agregar los beneficios")
+    private String beneficios;
+    @Valid
+    @NotNull(message = "El id debe ser valido")
+    private Pagos Pago;
 }
--- ms_planes/src/main/java/com/proyectogimnasio/planes/dto/PlanesResponse.java
+++ ms_planes/src/main/java/com/proyectogimnasio/planes/dto/PlanesResponse.java
@@ -1,4 +1,18 @@
 package com.proyectogimnasio.planes.dto;
 
+import com.proyectogimnasio.planes.model.Pagos;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PlanesResponse {
+    private Long id;
+    private String nombrePlan;
+    private Integer precioPlan;
+    private String descripcionPlan;
+    private String beneficios;
+    private Pagos Pagos;
+
 }
