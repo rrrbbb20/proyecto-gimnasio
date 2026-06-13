5839bed3fccd88e2dcac7af08d27570d6fcca0e9
krist41 <kristine.spl@proton.me>
2026-06-13 17:44:24 -0400

    Terminado el controller de rutina, ejercicio y detallesejercicio, con swagger y hateoas, aparte de arreglar una respuesta en pagoscontroller

--- ms-rutina/src/main/java/com/proyectogimnasio/rutina/controller/DetallesEjercicioController.java
+++ ms-rutina/src/main/java/com/proyectogimnasio/rutina/controller/DetallesEjercicioController.java
@@ -1,4 +1,133 @@
 package com.proyectogimnasio.rutina.controller;
 
+import com.proyectogimnasio.rutina.dto.*;
+import com.proyectogimnasio.rutina.service.RutinaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/api/v1/detallesejercicios")
+@RequiredArgsConstructor
 public class DetallesEjercicioController {
+    private final RutinaService service;
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+
+    @Operation(
+            summary = "Agregar un detalles de las rutinas",
+            description = "Agrega un detalles de las rutinas. Requiere rol ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Detalles Creados"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+
+    public ResponseEntity<ApiResponse<DetallesEjercicioResponse>> addDetalles(@Valid @RequestBody DetallesEjercicioRequest d, String token ){
+        return ResponseEntity.status(201).body(
+                ApiResponse.<DetallesEjercicioResponse>builder().success(true)
+                        .message("Detalles creados")
+                        .data(service.addDetalles(d, token)).build()
+
+        );
+    }
+    @Operation(
+            summary = "Obtener detalles de las rutinas por ID",
+            description = "Busca un detalles de las rutinas usando su id. Requiere rol USER o ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Detalles obtenidos"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Detalles no encontrados"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<ApiResponse<EntityModel<DetallesEjercicioResponse>>> findDetalles(@PathVariable("id")Long id, String token){
+        DetallesEjercicioResponse detalles = service.findDetalles(id, token);
+        EntityModel<DetallesEjercicioResponse> recurso = EntityModel.of(detalles);
+
+        recurso.add(linkTo(methodOn(DetallesEjercicioController.class).findDetalles(id, token)).withSelfRel());
+        recurso.add(linkTo(methodOn(DetallesEjercicioController.class).getDetalles(token)).withRel("all"));
+        recurso.add(linkTo(methodOn(DetallesEjercicioController.class).updateDetalles(id, null, token)).withRel("update"));
+        recurso.add(linkTo(methodOn(DetallesEjercicioController.class).deleteDetalles(id)).withRel("delete"));
+
+        return ResponseEntity.ok(
+                ApiResponse.<EntityModel<DetallesEjercicioResponse>>builder()
+                        .success(true)
+                        .message("Detalles obtenidos")
+                        .data(recurso)
+                        .build()
+        );
+
+    }
+    @Operation(
+            summary = "Listar detalles de rutinas",
+            description = "Retorna todos los detalles de rutinas registrados. Requiere rol USER o ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Listado obtenido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+    @GetMapping
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<ApiResponse<List<DetallesEjercicioResponse>>> getDetalles(String token){
+        return ResponseEntity.status(200).body(
+                ApiResponse.<List<DetallesEjercicioResponse>>builder().success(true)
+                        .data(service.getDetalles(token)).build()
+        );
+
+    }
+    @Operation(
+            summary = "Actualizar detalles de las rutinas por ID ",
+            description = "Busca un detalles de las rutinas usando su id. Requiere rol USER o ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Detalles Actualizados"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Detalles no encontrados"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse<DetallesEjercicioResponse>> updateDetalles(@PathVariable("id") Long id, @Valid @RequestBody DetallesEjercicioRequest d, String token) {
+        return ResponseEntity.ok(
+
+                ApiResponse.<DetallesEjercicioResponse>builder().success(true)
+                        .data(service.updateDetalles(id,d, token)).build()
+
+        );
+
+    }@Operation(
+            summary = "Eliminar detalles por ID",
+            description = "Elimina detalles de las rutinas usando su identificador. Requiere rol ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "Detalles eliminado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Detalles no encontrado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse<Void>> deleteDetalles(@PathVariable("id") Long id){
+        service.deleteDetalles(id);
+        return ResponseEntity.ok(
+
+                ApiResponse.<Void>builder().success(true).message("Detalles Eliminados").build()
+        );
+    }
+
 }
--- ms-rutina/src/main/java/com/proyectogimnasio/rutina/controller/RutinaController.java
+++ ms-rutina/src/main/java/com/proyectogimnasio/rutina/controller/RutinaController.java
@@ -1,4 +1,138 @@
 package com.proyectogimnasio.rutina.controller;
 
+import com.proyectogimnasio.rutina.dto.ApiResponse;
+import com.proyectogimnasio.rutina.dto.RutinaResponse;
+import com.proyectogimnasio.rutina.dto.RutinaRequest;
+import com.proyectogimnasio.rutina.service.RutinaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/api/v3/rutinas")
+@RequiredArgsConstructor
 public class RutinaController {
+    private final RutinaService service;
+    @Operation(
+            summary = "Agregar una rutina",
+            description = "Agrega una rutina. Requiere rol ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Rutina Creada"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse<RutinaResponse>> addRutinas(@Valid @RequestBody RutinaRequest r, String token){
+
+        return ResponseEntity.status(201).body(
+                ApiResponse.<RutinaResponse>builder().success(true)
+                        .message("Rutina creada")
+                        .data(service.addRutina(r, token)).build()
+
+        );
+
+    }
+    @Operation(
+            summary = "Obtener rutina por ID",
+            description = "Busca una rutina usando su id. Requiere rol USER o ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Rutina obtenida"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Rutina no encontrada"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<ApiResponse<EntityModel<RutinaResponse>>> findRutinas(@PathVariable Long id, String token){
+        RutinaResponse rutina = service.findRutina(id, token);
+        EntityModel<RutinaResponse> recurso = EntityModel.of(rutina);
+
+        recurso.add(linkTo(methodOn(RutinaController.class).findRutinas(id, token)).withSelfRel());
+        recurso.add(linkTo(methodOn(RutinaController.class).getRutinas(token)).withRel("all"));
+        recurso.add(linkTo(methodOn(RutinaController.class).updateRutinas(id, null, token)).withRel("update"));
+        recurso.add(linkTo(methodOn(RutinaController.class).deleteRutinas(id)).withRel("delete"));
+
+        return ResponseEntity.ok(
+                ApiResponse.<EntityModel<RutinaResponse>>builder()
+                        .success(true)
+                        .message("Rutina obtenidos")
+                        .data(recurso)
+                        .build()
+        );
+    }
+    @Operation(
+            summary = "Listar rutinas",
+            description = "Retorna todas las rutinas registrados. Requiere rol USER o ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Listado obtenido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+    @GetMapping
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<ApiResponse<List<RutinaResponse>>> getRutinas(String token){
+
+        return ResponseEntity.status(200).body(
+                ApiResponse.<List<RutinaResponse>>builder().success(true)
+                        .data(service.getRutinas(token)).build()
+        );
+
+    }
+    @Operation(
+            summary = "Actualizar rutina por ID ",
+            description = "Busca una rutina usando su id. Requiere rol USER o ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Rutina Actualizada"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Rutina no encontrada"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse<RutinaResponse>> updateRutinas(@PathVariable Long id, @Valid @RequestBody RutinaRequest r, String token) {
+
+        return ResponseEntity.ok(
+
+                ApiResponse.<RutinaResponse>builder().success(true)
+                        .data(service.updateRutina(id,r, token)).build()
+
+        );
+
+    }
+    @Operation(
+            summary = "Eliminar rutina por ID",
+            description = "Elimina una rutina usando su identificador. Requiere rol ADMIN."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "Rutina eliminada"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Rutina no encontrada"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autenticado o token inválido"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Acceso denegado")
+    })
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse<Void>> deleteRutinas(@PathVariable Long id){
+        service.deleteRutina(id);
+        return ResponseEntity.ok(
+
+                ApiResponse.<Void>builder().success(true).message("Rutina Eliminado").build()
+        );
+    }
 }
