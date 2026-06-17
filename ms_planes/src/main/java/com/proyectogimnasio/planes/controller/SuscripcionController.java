a25186c0c98c98d0379511d5cc144ce543dfa24a
krist41 <kristine.spl@proton.me>
2026-06-17 14:07:00 -0400

    subir la version de suscripciones para que queden parejos

--- ms_planes/src/main/java/com/proyectogimnasio/planes/controller/SuscripcionController.java
+++ ms_planes/src/main/java/com/proyectogimnasio/planes/controller/SuscripcionController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/suscripciones")
+@RequestMapping("/api/v3/suscripciones")
 @RequiredArgsConstructor
 public class SuscripcionController {
 
