3d77284ae392b80b8161d50232ab3c116f41b8e6
krist41 <kristine.spl@proton.me>
2026-06-17 14:06:08 -0400

    cambio del client de planes para usar el nuevo url y que funcione correctamente

--- ms-cliente/src/main/java/com/proyectogimnasio/cliente/client/PlanesClient.java
+++ ms-cliente/src/main/java/com/proyectogimnasio/cliente/client/PlanesClient.java
@@ -3,19 +3,24 @@ package com.proyectogimnasio.cliente.client;
 import com.proyectogimnasio.cliente.dto.ApiResponse;
 import com.proyectogimnasio.cliente.dto.PlanesResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
-@RequiredArgsConstructor
 public class PlanesClient {
     private final WebClient webClient;
-    private final String BASE_URL = "http://localhost:8087/api/v3/planes/";
+    private final String baseUrl;
+
+    public PlanesClient(WebClient webClient, @Value("${planes.service.url:http://planes:8087}") String baseUrl) {
+        this.webClient = webClient;
+        this.baseUrl = baseUrl;
+    }
 
     public PlanesResponse getPlan(Long id) {
         ApiResponse<PlanesResponse> response = webClient.get()
-                .uri(BASE_URL + id)
+                .uri(baseUrl + "/api/v3/planes/" + id)
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<ApiResponse<PlanesResponse>>() {})
                 .block();
@@ -25,7 +30,7 @@ public class PlanesClient {
 
     public Object activarSuscripcion(Object suscripcionRequest) {
         return webClient.post()
-                .uri("http://localhost:8087/api/v1/suscripciones")
+                .uri(baseUrl + "/api/v3/suscripciones")
                 .bodyValue(suscripcionRequest)
                 .retrieve()
                 .bodyToMono(Object.class)
